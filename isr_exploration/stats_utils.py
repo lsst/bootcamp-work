bb704908b8b0c8652d95def52181c3fab63589e1
Eric Charles <echarles@slac.stanford.edu>
2018-11-15 20:19:22 +0000

    Rework status utils to table astropy table as input

--- isr_exploration/stats_utils.py
+++ isr_exploration/stats_utils.py
@@ -13,31 +13,50 @@ def single_gaussian_fit(params, x, y):
     return (fit - y)
 
 
-def bin_lists(input_dict, bin_dict):
+def bin_lists(intable, bin_dict):
     o_dict = {}
     stats_dict = {}
-    for key, val_list in input_dict.items():
+    for key, binning in bin_dict.items():
         try:
-            xmin, xmax, nbins = bin_dict[key]
+            data = intable[key].data
         except KeyError:
             continue
+        xmin, xmax, nbins = binning
+        if xmin is None:
+            xmin = np.min(data)
+        if xmax is None:
+            xmax = np.min(data)
         bins = np.linspace(xmin, xmax, nbins+1)
-        hist = np.histogram(val_list, bins=bins)
-        if len(val_list) > 0:
-            mean = np.mean(val_list)
-            stdev = np.std(val_list)        
+        hist = np.histogram(data, bins=bins)
+        if len(data) > 0:
+            mean = np.mean(data)
+            stdev = np.std(data)
+        else:
+            mean = 0.
+            stdev = 0.
+            hist = None
+        o_dict[key] = dict(hist=hist,
+                           mean=mean,
+                           stdev=stdev)
+    return o_dict
+
+
+def fit_hists_gaussian(hist_dict, keys):
+
+    for key in keys:
+        data = hist_dict[key]
+        hist = data['hist']
+        if hist is None:
+            fit = None
+            model = None
+        else:
+            mean = data['mean']
+            stdev = data['stdev']
             y = hist[0]
             x = [(hist[1][i]+hist[1][i+1])/2 for i in range(len(hist[1])-1)]
             norm = np.sum(y)
             pars = (norm, mean, stdev)
             fit = leastsq(single_gaussian_fit, pars, args=(x, y))
-        else:
-            mean = 0.
-            stdev = 0.
-            fit = None
-        o_dict[key] = dict(hist=hist,
-                           mean=mean,
-                           stdev=stdev,
-                           fit=fit)
-    return o_dict
-
+            model = single_gaussian(x, fit[0])
+        hist_dict[key]['fit'] = fit
+        hist_dict[key]['model'] = model
